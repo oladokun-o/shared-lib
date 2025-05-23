25814f11aa891fd52c2dcac80f98d6665d52950d
Oladokun <65833243+oladokun-o@users.noreply.github.com>
2025-05-23 07:26:37 +0100

    feat: add winston logger with custom levels and transports

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,6 +1,47 @@
-export const logger = {
-  info: console.log,
-  error: console.error,
-  warn: console.warn,
-  debug: console.debug,
+import winston from "winston";
+
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  debug: 4,
 };
+
+const level = (env: string | undefined) => {
+  return env === "development" ? "debug" : "info";
+};
+
+const colors = {
+  error: "red",
+  warn: "yellow",
+  info: "green",
+  http: "magenta",
+  debug: "blue",
+};
+
+winston.addColors(colors);
+
+const format = winston.format.combine(
+  winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss:ms" }),
+  winston.format.colorize({ all: true }),
+  winston.format.printf(
+    (info) => `${info.timestamp} ${info.level}: ${info.message}`
+  )
+);
+
+const transports = [
+  new winston.transports.Console(),
+  new winston.transports.File({
+    filename: "logs/error.log",
+    level: "error",
+  }),
+  new winston.transports.File({ filename: "logs/all.log" }),
+];
+
+export const logger = (env: string | undefined) => winston.createLogger({
+  level: level(env),
+  levels,
+  format,
+  transports,
+});
